9754f8abfd7358e2a93b7930105741b1612cb51e
kiranurs4ever <kiranurs4ever@gmail.com>
2022-04-06 13:17:16 +0530

    Java sample problems

---
+++ src/Swap2.java
@@ -0,0 +1,22 @@
+
+public class Swap2 {
+	
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int t;
+		/*
+		t=a;
+		a=b;
+		b=t;
+		*/
+		
+		a=a+b;
+		b= a-b;
+		a=a-b;
+		
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
